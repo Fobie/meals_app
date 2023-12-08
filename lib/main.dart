f93a25d1703237cf27eeddf22a38b8a1c6605124
Win Khant Paing <wkhant047@gmail.com>
2023-12-08 12:01:55 +0630

    Updated 8/12/23

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
           brightness: Brightness.dark,
-          seedColor: const Color.fromARGB(255, 131, 57, 0),
+          seedColor: const Color.fromARGB(255, 131, 50, 0),
         ),
         textTheme: GoogleFonts.latoTextTheme(),
       ),
